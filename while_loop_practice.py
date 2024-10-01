ee18be4916b1f34240a44ae99a9bff52309c27a0
JayDaLion <Jaystuff123@gmail.com>
2024-10-01 11:20:19 -0700

    main

---
+++ while_loop_practice.py
@@ -0,0 +1,36 @@
+"""#marshmellow example
+marshmallows = 4
+while marshmallows < 5:
+    marshmallows += 1
+    print("Added a marshmellow! Now there are " + str(marshmallows) + " marshmallows.")"""
+
+"""#down we go
+steps = 40
+
+while steps > 0:
+    print("descending the stairs, "+ str(steps) + " steps remaing")
+    steps -= 1
+print("we reached the bottom")"""
+
+"""#The final countdown 
+set_time = 40
+
+while set_time > 0:
+    set_time -= 1
+    print(f"Theres {set_time} seconds left")"""
+
+#patient que
+patients = 20
+while patients > 0:
+    patients -= 2
+    print(f"There are {patients} patients Left to take care of!!!")
+
+"""#Battery charger x efficiency check
+battery_percentage = 85
+is_over_80 = battery_percentage
+charge_type = ["Bulk", "Absorption", "Float"]
+
+while battery_percentage < 100:
+    if is_over_80 > 80:
+        print(f"Battery is at {battery_percentage} supplying {charge_type[2]}!!!") 
+    break"""
